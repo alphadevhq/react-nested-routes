135e187cfe9fdac8109f3d6232f168a14f92788f
Abdhesh Nayak <abdheshnayak@gmail.com>
2021-11-26 19:49:54 +0530

    :tada: init

---
+++ src/index.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { useLocation } from 'react-router';
+
+const NestedRoute = (props) => {
+  const { children, path, parent } = props;
+  const history = useLocation();
+  const pathname = history.pathname;
+  const pathHistory = pathname.split('/');
+
+  const parentIndex = pathHistory.indexOf(parent);
+
+  let match;
+
+  if (parentIndex !== -1) {
+    match = path === pathHistory[parentIndex + 1];
+  }
+
+  return match ? children : <React.Fragment />;
+};
+
+export const useActivePath = ({ parent }) => {
+  const history = useLocation();
+  const pathname = history.pathname;
+  const pathHistory = pathname.split('/');
+
+  const parentIndex = pathHistory.indexOf(parent);
+
+  let activeTab;
+  if (parentIndex !== -1) {
+    activeTab = pathHistory[parentIndex + 1];
+  }
+
+  return activeTab;
+};
+
+export default NestedRoute;
